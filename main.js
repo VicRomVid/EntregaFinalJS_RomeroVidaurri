3a15e2a35e6e231658f5bc19cb58365c58fdea3f
VicRomVid <vicromvid@gmail.com>
2023-09-02 16:13:42 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+let lista = []
+
+let elementoAgregar = document.getElementById("botonAgregar");
+
+let elementoGuardar = document.getElementById("botonGuardar");
+
+let elementoHistorica = document.getElementById("botonHistorica");
+
+elementoAgregar.addEventListener("click",
+    function agregarALista(){
+    const especie = document.getElementById("especie").value
+    const observaciones = parseInt(document.getElementById("observaciones").value)
+    if(!isNaN(observaciones) && observaciones >0){
+        const item = {especie, observaciones};
+        lista.push(item);
+        mostrarLista();
+    }else{
+        alert("Ingresa una cantidad válida")
+    }
+});
+
+elementoGuardar.addEventListener("click",
+    function guardarLista(){
+    const listaJSON = JSON.stringify(lista);
+    localStorage.setItem("listaData", listaJSON)
+    alert("Lista guardada en el localstorage")
+});
+
+elementoHistorica.addEventListener("click",
+function historica(){
+    fetch('listaHistorica.json')
+    .then(response => response.json())
+    .then(info => {
+        const aves = info.aves;
+        const avesContainer = document.getElementById('lista');
+    
+        aves.forEach(ave => {
+            const aveElement = document.createElement('li');
+            aveElement.textContent = `Nombre Común: ${ave.nombreComun}, Nombre Científico: ${ave.nombreCient}, Residencia: ${ave.residencia}`
+            avesContainer.appendChild(aveElement);
+        });
+    })
+    .catch(error => {
+        console.error('Oh-oh! Algo salió mal.', error);
+    });
+});
+
+function mostrarLista() {
+    const listaElement = document.getElementById("lista")
+    listaElement.innerHTML="";
+
+    lista.forEach((item)=>{
+        const listItem = document.createElement("li")
+        listItem.textContent = `${item.especie} - observaciones: ${item.observaciones}`
+        listaElement.appendChild(listItem)
+    })  
+};
+
